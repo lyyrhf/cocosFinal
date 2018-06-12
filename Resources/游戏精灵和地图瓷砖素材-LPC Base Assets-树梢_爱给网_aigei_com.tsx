80eba9304cffdfa5d40a0275784f462d19a39b5c
yiqu zhu <kotomineshiki@outlook.com>
2018-06-12 22:21:31 +0800

    地图

---
+++ Resources/游戏精灵和地图瓷砖素材-LPC Base Assets-树梢_爱给网_aigei_com.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="游戏精灵和地图瓷砖素材-LPC Base Assets-树梢_爱给网_aigei_com" tilewidth="40" tileheight="40" tilecount="4" columns="2">
+ <image source="游戏精灵和地图瓷砖素材-LPC Base Assets-树梢_爱给网_aigei_com.png" width="96" height="112"/>
+</tileset>
---
+++ Resources/砖.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="砖" tilewidth="40" tileheight="40" tilecount="12" columns="12">
+ <image source="砖.png" width="480" height="40"/>
+</tileset>
